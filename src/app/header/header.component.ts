57e1fba453c0f378a85f3bb1cbc14e27360a3b99
Cyrille Bomba <cbngong@gmail.com>
2022-04-29 16:55:42 +0200

    Login/Logout ok + header on homepage

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(private auth: AuthService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  goToHome() {
+    this.router.navigate(['home']);
   }
 
+  logout() {
+    this.auth.logout();
+  }
 }
